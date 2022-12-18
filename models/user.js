f4830bda3c8739fcd07a9e7bccb690e63e5bd64d
Ksenia Frants <frants.ksenka@gmail.com>
2022-12-18 21:19:30 +0700

    Создать схемы и модели ресурсов API

---
+++ models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const { default: isEmail } = require('validator/lib/isEmail');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: [true, 'Поле "email" должно быть заполнено'],
+    validate: {
+      validator: (v) => isEmail(v),
+      message: 'Неправильный формат почты',
+    },
+  },
+  password: {
+    type: String,
+    select: false,
+    required: [true, 'Поле "password" должно быть заполнено'],
+  },
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+});
+
+module.exports = mongoose.model('user', userSchema);
